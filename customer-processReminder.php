92fb203da5386e0511c03ba440608a13d08a18ce
Claudio Christyo <mosint.nagant@gmail.com>
2020-06-26 13:43:25 +0700

    add customer-processReminder.php - 26/6/2020

---
+++ customer-processReminder.php
@@ -0,0 +1,9 @@
+<?php
+include_once('csv-import.php');
+////////////////////////////////////
+//Variables from csv-import.php : //
+//$arr_upload_file                //
+////////////////////////////////////
+
+
+?>
