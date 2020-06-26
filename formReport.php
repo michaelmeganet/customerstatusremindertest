f2ea3d84c5fe37dd678ef2dcd147eaf08241e520
Claudio Christyo <mosint.nagant@gmail.com>
2020-06-26 15:58:40 +0700

    revised function to accomodate multiple pages - 26/6/2020

--- formReport.php
+++ formReport.php
@@ -4,49 +4,36 @@ if(isset($_SESSION['tempName'])){
     $tempName = $_SESSION['tempName'];
 }
 ?>
+            <div class="container">
+            <table border="1" style="align-content: center;max-width: 2480px;width: 100%">
+                <thead>
+                    <tr>
+                        <th style="text-align: center;"colspan="2">THIS IS A TEST REPORT</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style="width:30%">
+                            Name&nbsp;&nbsp;: <?php echo $i;?><br>
+                            Class&nbsp;&nbsp;: XII-Social<br>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta nibh. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Faucibus turpis in eu mi bibendum neque. Nisl tincidunt eget nullam non nisi. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Pellentesque adipiscing commodo elit at imperdiet dui accumsan. Dolor sit amet consectetur adipiscing elit duis tristique. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor.
 
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <div class="container">
-        <table border="1" style="align-content: center;max-width: 2480px;width: 100%">
-            <thead>
-                <tr >
-                    <th style="text-align: center;"colspan="2">THIS IS A TEST REPORT</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td style="width:30%">
-                        Name&nbsp;&nbsp;: <?php echo $tempName;?><br>
-                        Class&nbsp;&nbsp;: XII-Social<br>
-                    </td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta nibh. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Faucibus turpis in eu mi bibendum neque. Nisl tincidunt eget nullam non nisi. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Pellentesque adipiscing commodo elit at imperdiet dui accumsan. Dolor sit amet consectetur adipiscing elit duis tristique. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor.
+    Etiam tempor orci eu lobortis elementum nibh tellus. Facilisis gravida neque convallis a. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer. Nunc non blandit massa enim nec dui nunc mattis enim. Neque sodales ut etiam sit amet nisl purus in mollis. Quisque egestas diam in arcu cursus euismod quis viverra. Nulla aliquet enim tortor at auctor urna nunc id. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Facilisi etiam dignissim diam quis enim lobortis scelerisque. Sed odio morbi quis commodo odio aenean sed adipiscing. Morbi enim nunc faucibus a pellentesque. Proin gravida hendrerit lectus a. Mi tempus imperdiet nulla malesuada pellentesque elit eget. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Enim nulla aliquet porttitor lacus luctus. Ac turpis egestas maecenas pharetra.
 
-Etiam tempor orci eu lobortis elementum nibh tellus. Facilisis gravida neque convallis a. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer. Nunc non blandit massa enim nec dui nunc mattis enim. Neque sodales ut etiam sit amet nisl purus in mollis. Quisque egestas diam in arcu cursus euismod quis viverra. Nulla aliquet enim tortor at auctor urna nunc id. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Facilisi etiam dignissim diam quis enim lobortis scelerisque. Sed odio morbi quis commodo odio aenean sed adipiscing. Morbi enim nunc faucibus a pellentesque. Proin gravida hendrerit lectus a. Mi tempus imperdiet nulla malesuada pellentesque elit eget. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Enim nulla aliquet porttitor lacus luctus. Ac turpis egestas maecenas pharetra.
+    Amet nulla facilisi morbi tempus iaculis. Augue eget arcu dictum varius duis at. Et netus et malesuada fames ac turpis. Nibh mauris cursus mattis molestie a iaculis at. Etiam tempor orci eu lobortis elementum nibh tellus molestie nunc. Morbi non arcu risus quis varius quam. Eu ultrices vitae auctor eu augue ut. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Vel pretium lectus quam id leo in. Etiam sit amet nisl purus in mollis nunc. Tellus mauris a diam maecenas sed enim ut sem viverra. Nibh tellus molestie nunc non blandit. Faucibus interdum posuere lorem ipsum dolor sit amet. Tincidunt vitae semper quis lectus nulla. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget. Est ultricies integer quis auctor elit sed vulputate. Ac orci phasellus egestas tellus rutrum tellus pellentesque. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Tortor at auctor urna nunc id cursus metus aliquam eleifend.
 
-Amet nulla facilisi morbi tempus iaculis. Augue eget arcu dictum varius duis at. Et netus et malesuada fames ac turpis. Nibh mauris cursus mattis molestie a iaculis at. Etiam tempor orci eu lobortis elementum nibh tellus molestie nunc. Morbi non arcu risus quis varius quam. Eu ultrices vitae auctor eu augue ut. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Vel pretium lectus quam id leo in. Etiam sit amet nisl purus in mollis nunc. Tellus mauris a diam maecenas sed enim ut sem viverra. Nibh tellus molestie nunc non blandit. Faucibus interdum posuere lorem ipsum dolor sit amet. Tincidunt vitae semper quis lectus nulla. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget. Est ultricies integer quis auctor elit sed vulputate. Ac orci phasellus egestas tellus rutrum tellus pellentesque. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Tortor at auctor urna nunc id cursus metus aliquam eleifend.
+    Metus aliquam eleifend mi in nulla. Accumsan sit amet nulla facilisi morbi tempus iaculis urna. Semper eget duis at tellus at urna condimentum mattis pellentesque. Ipsum a arcu cursus vitae congue mauris rhoncus. Elementum tempus egestas sed sed risus pretium quam. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel. Turpis egestas integer eget aliquet nibh praesent tristique. Morbi tempus iaculis urna id. Cras adipiscing enim eu turpis egestas pretium. Vitae semper quis lectus nulla at volutpat diam ut. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Vitae tortor condimentum lacinia quis. Fermentum iaculis eu non diam phasellus. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio.
 
-Metus aliquam eleifend mi in nulla. Accumsan sit amet nulla facilisi morbi tempus iaculis urna. Semper eget duis at tellus at urna condimentum mattis pellentesque. Ipsum a arcu cursus vitae congue mauris rhoncus. Elementum tempus egestas sed sed risus pretium quam. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel. Turpis egestas integer eget aliquet nibh praesent tristique. Morbi tempus iaculis urna id. Cras adipiscing enim eu turpis egestas pretium. Vitae semper quis lectus nulla at volutpat diam ut. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Vitae tortor condimentum lacinia quis. Fermentum iaculis eu non diam phasellus. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio.
+    Ut tellus elementum sagittis vitae et. Egestas congue quisque egestas diam. Blandit massa enim nec dui nunc mattis enim. Elementum integer enim neque volutpat ac. Pellentesque id nibh tortor id aliquet lectus proin nibh. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Elementum eu facilisis sed odio morbi. Suspendisse potenti nullam ac tortor vitae purus faucibus. Tellus orci ac auctor augue mauris augue neque gravida. Urna molestie at elementum eu facilisis sed odio morbi. Ornare quam viverra orci sagittis eu volutpat odio facilisis. In arcu cursus euismod quis viverra nibh cras. Faucibus turpis in eu mi. Et pharetra pharetra massa massa ultricies mi quis. Hendrerit gravida rutrum quisque non tellus orci ac auctor. Placerat vestibulum lectus mauris ultrices eros in cursus. Integer vitae justo eget magna fermentum iaculis eu non. At risus viverra adipiscing at. Eu lobortis elementum nibh tellus molestie nunc non.
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+      
 
-Ut tellus elementum sagittis vitae et. Egestas congue quisque egestas diam. Blandit massa enim nec dui nunc mattis enim. Elementum integer enim neque volutpat ac. Pellentesque id nibh tortor id aliquet lectus proin nibh. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Elementum eu facilisis sed odio morbi. Suspendisse potenti nullam ac tortor vitae purus faucibus. Tellus orci ac auctor augue mauris augue neque gravida. Urna molestie at elementum eu facilisis sed odio morbi. Ornare quam viverra orci sagittis eu volutpat odio facilisis. In arcu cursus euismod quis viverra nibh cras. Faucibus turpis in eu mi. Et pharetra pharetra massa massa ultricies mi quis. Hendrerit gravida rutrum quisque non tellus orci ac auctor. Placerat vestibulum lectus mauris ultrices eros in cursus. Integer vitae justo eget magna fermentum iaculis eu non. At risus viverra adipiscing at. Eu lobortis elementum nibh tellus molestie nunc non.
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        </div>
-    </body>
-</html>
--- testPrint.php
+++ testPrint.php
@@ -8,24 +8,42 @@ require 'vendor/autoload.php'; //required, this is to autoload composer.
 use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
+// https://github.com/dompdf/dompdf#quick-start
 $dompdf = new Dompdf();
-ob_start();
-require('./formReport.php');
-$dompdf->set_option('isHtml5ParserEnabled', true);
-$html = ob_get_contents();
-ob_get_clean();
-#$html = file_get_contents("./formReport.php");
-$dompdf->loadHtml($html);
+    ob_start();  //reference for using output buffer : https://stackoverflow.com/questions/50695307/render-html-with-dynamic-variable-values-with-dompdf
+    for ($i = 0; $i < 3; $i++){
+        switch ($i){
+            case '0':
+                echo "<div>";
+                break;
+            default:
+                echo "<div style='page-break-before: always'>";
+                break;
+        }
+        ?>
+            <?php
+            #include "formReport.php";
+            include "letterhtml2.html";
+            ?>
+        </div>
+        <?php
+    }
+    #require('./formReport.php');
+    $dompdf->set_option('isHtml5ParserEnabled', true);
+    $html = ob_get_contents();
+    ob_get_clean();
+    #$html = file_get_contents("./formReport.php");
+    $dompdf->loadHtml($html);
 
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+    // (Optional) Setup the paper size and orientation
+    $dompdf->setPaper('A4', 'portrait');
 
-// Render the HTML as PDF
-$dompdf->render();
+    // Render the HTML as PDF
+    $dompdf->render();
+
 
 // Output the generated PDF to Browser
 $dompdf->stream("download1",array('Attachment' => 0));
-
 
 /*
 #include "./assets/phpwkhtmltopdf/src/Pdf.php";
